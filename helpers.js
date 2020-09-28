42e98a0ecd48ea2645ee47bab9c611cc4ca754ef
igorurr <igorurr@yandex.ru>
2020-09-28 15:07:03 +0300

    df

--- helpers.js
+++ helpers.js
@@ -1,5 +1,8 @@
 const { spawnSync } = require('child_process');
 
+/*
+    выполняет команду не выводя её результат в консоль
+*/
 const sysCall = comand => {
   const [ cmd, ...comandArgs ] = comand.split(' ')
   
@@ -11,6 +14,34 @@ const sysCall = comand => {
   });
 }
 
+const sysCallOut = comand => {
+  const [ cmd, ...comandArgs ] = comand.split(' ')
+  
+  spawnSync(cmd, comandArgs, {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: 'inherit',
+    encoding: 'utf-8'
+  });
+}
+
+const getComandLineArgs = () => Object.fromEntries(
+  process.argv.slice(2)
+    .map(arg => arg.match(/^(--[^\s]*)$/))
+    .filter(arg => arg)
+    .map(arg => arg[0].split('='))
+    .map(([key, value]) => [key.slice(2), value || true])
+)
+
+// см. https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+const consoleLog = {
+  error: msg => console.log('\x1b[31m%s\x1b[0m', msg),
+  info: msg => console.log('\x1b[36m%s\x1b[0m', msg),
+}
+
 module.exports = {
-  sysCall
+  sysCall,
+  sysCallOut,
+  getComandLineArgs,
+  consoleLog
 }
--- testComandLine.js
+++ testComandLine.js
@@ -1,11 +1,24 @@
-const { sysCall } = require('./helpers');
+const { sysCall, sysCallOut, getComandLineArgs, consoleLog } = require('./helpers');
 
 const gitStatusResponse = sysCall('git status --porcelain').stdout
 
 const wasChanges = gitStatusResponse.split('\n').length > 1
 
-if (wasChanges) {
+const acceptChanges = getComandLineArgs()['accept-changes']
+
+const showNoComitchanges = () => sysCallOut('git status')
+
+if (wasChanges && !acceptChanges) {
+  consoleLog.error('Есть незакоммиченные изменения')
+  showNoComitchanges()
   process.exit(1)
 }
+else if (wasChanges && acceptChanges) {
+  consoleLog.info('Есть незакоммиченные изменения')
+  showNoComitchanges()
+}
+else {
+  consoleLog.info('Незакоммиченные изменения отсутствуют')
+}
 
-console.log('asasas', wasChanges, gitStatusResponse.split('\n'))
+process.exit(0)
