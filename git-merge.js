d9c1d4ea1b63b7913fa79ef6964ade99ba153a6b
igorurr <igorurr@yandex.ru>
2020-09-29 16:31:57 +0300

    cvx

--- git-merge.js
+++ git-merge.js
@@ -5,7 +5,12 @@ const brunchName = getComandLineArgs()[0]
 process.exit(
   sysCallOut(
     makeMainAndSubmodulesComand(
-      `node \`${process.cwd()}/git-check-conflicts.js\` ${brunchName} && git merge ${brunchName}`
+      // чекаем конфликты, если они есть, обрываемся
+      // если их нет то делаем мёрдж, он отработает только в репо без сабмодулей, отработает всегда
+      // если не отработал, значит в репо есть сабмодули, их можно просто добавить 
+      // к комиту и закомитить мёрдж-коммит
+      `node \`${process.cwd()}/git-check-conflicts.js\` ${brunchName} && ` +
+      `(git merge ${brunchName} || (git add -A && git commit))`
     )
   ).status
 )
